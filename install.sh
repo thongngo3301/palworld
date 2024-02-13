79ae6a52e366213e39457f1a74b5d9a5ff53d227
thongngo3301 <thongngo3301@gmail.com>
2024-02-13 15:30:12 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 export PALWORLD_DIR=/opt/stacks/palworld
 curl -fsSL https://raw.githubusercontent.com/thongngo3301/palworld/master/.env > ${PALWORLD_DIR}/.env
-curl -fsSL https://github.com/thongngo3301/palworld/raw/master/docker-compose.yaml > ${PALWORLD_DIR}/docker-compose.yaml
+curl -fsSL https://raw.githubusercontent.com/thongngo3301/palworld/master/docker-compose.yaml > ${PALWORLD_DIR}/docker-compose.yaml
